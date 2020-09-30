892be920017ed3b6274bba4fd63d830f0f277c9e
Glitch (hello-webpage) <none>
2020-09-30 12:20:26 +0000

    🏉🍞 Checkpoint ./index.html:17680986/572 ./script.js:17680986/685 ./style.css:17680986/4

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+window.onload = function() {
+  if(window.innerWidth > window.innerHeight)
+};
 window.addEventListener("mousemove", e => {
-  let percentage = e.clientX / window.innerWidth * 100;
-  document.querySelector("#moon-shadow").style.left = `${percentage}%`;
+  let percentage = (e.clientX / window.innerWidth) * 100;
+  document.querySelector(".moon-shadow").style.left = `${percentage}%`;
 });
