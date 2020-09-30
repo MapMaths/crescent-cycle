5fc56d26a83ac2712dbde005109224d09ca2493c
Glitch (hello-webpage) <none>
2020-09-30 12:31:28 +0000

    🐙⚡️ Checkpoint ./script.js:17680986/297 ./vrtest/index.html:17680986/4326 ./style.css:17680986/84

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var moonBody = document.querySelector(".moon-body");
+var moonShadow = document.querySelector(".moon-shadow")
 window.onload = function() {
-  if(window.innerWidth > window.innerHeight)
+  let r;
+  if(window.innerWidth >= window.innerHeight){
+    r = window.innerHeight;
+  } else {
+    r = window.innerWidth;
+  }
+  document.querySelector(".moon-shadow").style
 };
 window.addEventListener("mousemove", e => {
   let percentage = (e.clientX / window.innerWidth) * 100;
