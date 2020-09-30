796ba07dc9d04394d55fce3a36e837ed2b42bd37
Glitch (hello-webpage) <none>
2020-09-30 13:05:36 +0000

    💿👫 Checkpoint ./script.js:17680986/148 ./index.html:17680986/24 ./vrtest/index.html:17680986/23853

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 var moonBody = document.querySelector(".moon-body");
 var moonShadow = document.querySelector(".moon-shadow");
-window.onload = resize();
-window.onresize = resize();
-function resize() {
+window.onload = function() {
   let r;
   if (window.innerWidth >= window.innerHeight) {
     r = window.innerHeight / 2;
@@ -13,6 +11,9 @@ function resize() {
   moonShadow.style.height = `${r}px`;
   moonBody.style.width = `${r}px`;
   moonBody.style.height = `${r}px`;
+}
+window.onresize = function() {
+  window.location.reload();
 }
 window.addEventListener("mousemove", e => {
   let x = (e.clientX / window.innerWidth) * 100;
